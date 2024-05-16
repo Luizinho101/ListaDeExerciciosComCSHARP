0ee57c541623949fd03a21cd040763f2709f22d5
Luiz Antonio <luiz.antonio.da.silva777@gmail.com>
2024-05-16 15:02:44 -0300

    Adição do projeto com alguns exercicios

---
+++ ListaDeExercicios/02/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string nome;
+Console.WriteLine("Qual é o seu nome?");
+nome = Console.ReadLine();
+Console.WriteLine($"Olá ,{nome} é um prazer te conhecer!");
---
+++ ListaDeExercicios/03/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nome;
+        decimal salario;
+
+        Console.WriteLine("Informe seu nome!");
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Informe o seu salário!");
+        salario = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Nome do funcionário: {nome}");
+        Console.WriteLine($"Salário : {salario.ToString("C2")}");
+    }
+        
+}
